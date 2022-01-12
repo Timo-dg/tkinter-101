c1e1aea8187d27927337b87f09881652a3625218
Timo de Gruiter <timo@EDGITNEW.local>
2022-01-12 11:51:33 +0100

    clean up

--- grabbelton.py
+++ grabbelton.py
@@ -10,7 +10,6 @@ geld = ['1 euro', '5 euro', '10 euro', '50 euro', '100 euro','250 euro','500 eur
 gegrabbeld = ''
 counter = 10
 
-
 def grabbel():
     global gegrabbeld, counter
     gegrabbeld = random.choice(geld)
@@ -22,9 +21,6 @@ def grabbel():
     )
     messagebox.showwarning('Geld','Gefeliciteerd, je hebt '+ (gegrabbeld) +' gegrabbeld!')
     Frame.pack()
-
-
-
 
 button = tk.Button(
     text=('Grabbel'),
